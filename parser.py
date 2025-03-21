c559c09cf713d3fe27da7540c02f8204bfcee809
PedroAlb-Blip <pedroalbendeaperez@gmail.com>
2025-03-21 20:31:14 +0100

    modified:   parser.py

--- parser.py
+++ parser.py
@@ -1,8 +1,6 @@
 import subprocess
 import os
 from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
 from collections import defaultdict
 
 def tmp_rm():
@@ -10,7 +8,7 @@ def tmp_rm():
       check=str("aln_tmp_" + str(i))
       if os.path.exists(check):
          os.remove(check)
-   return check
+   return 
 
 tmp_rm()
 
@@ -19,11 +17,11 @@ def reader (path, fmt="abi"):
    ### This function takes the path of the forward or reverse, extracts the sequences and produces an object containing the fasta sequence
    record = SeqIO.read(path, fmt)
    if "-R_" in path:
-      sequence = str(record.annotations["abif_raw"]["PBAS1"]).replace("b","").replace("'","")[::-1]
+      sequence = str(record.annotations["abif_raw"]["PBAS2"]).replace("b","").replace("'","")[::-1]
       sequence=sequence.replace("A","Z").replace("G","X").replace("T","A").replace("C","G").replace("Z","T").replace("X","C")
       sequence = str(">Reverse" + '\n' + sequence + '\n')
    elif "-F_" in path:
-      sequence = str(record.annotations["abif_raw"]["PBAS1"]).replace("b","").replace("'","")
+      sequence = str(record.annotations["abif_raw"]["PBAS2"]).replace("b","").replace("'","")
       sequence = str(">Forward" + '\n' + sequence +'\n')
    return sequence
 
@@ -44,10 +42,12 @@ def tmp_aln(fw, rv):
 
 # tmp_aln(reader(path_fw), reader(path_rv, True))
 
-def aligner(file):
+def aligner(file, v=False):
    cmd = [r"C:\Mafft\mafft.bat", "--clustalout", "--localpair", "--maxiterate", "1000", "--op", "2.0", "--ep", "0.1", file]
    process = subprocess.run(cmd, capture_output=True, text=True)
    matches = process.stdout.split('\n')
+   if v==True:
+      print(process.stdout)
    fw_seq=""
    rv_seq=""
    match_seq=""
@@ -62,15 +62,13 @@ def aligner(file):
    return match_seq, fw_seq, rv_seq
 
 
-def modifier(path_fw, path_rv):
+def locator(path_fw, path_rv):
    all=aligner(tmp_aln(reader(path_fw), reader(path_rv)))
    align=all[0]
    fw=all[1]
    rv=all[2]
    Forward=reader(path_fw).removeprefix(">Forward\n")
    Reverse=reader(path_rv).removeprefix(">Reverse\n")
-   print(Forward)
-   print(Reverse)
    i=0
    while i < (len(align)-20):
       kmer=align[i:i+20]
@@ -84,10 +82,13 @@ def modifier(path_fw, path_rv):
          i=i+1
    return pos_1, pos_2
 
+##### VERY IMPORTANT: POSITIONS OF PLOC CHANGE RELATIVE TO THE SEQUENCE WHEN DOING A REVERSE COMPLEMENT 
+##### Perchance doing the absolute value of the subtraction between the PLOCs and the total length 14000
+
 path_fw="c:\\Users\\Pedro\\Downloads\\secuenciasvp7_sp101bsp105sp106sp109sp111sp113sp116\\sec2025-016_60_Sp101b-VP7_RV-VP7-F_2025-02-24.ab1"
 path_rv="c:\\Users\\Pedro\\Downloads\\secuenciasvp7_sp101bsp105sp106sp109sp111sp113sp116\\sec2025-016_91_Sp101b-VP7_RV-VP7-R_2025-02-24.ab1"
 #print(aligner(tmp_aln(reader(path_fw), reader(path_rv))))
-print(modifier(path_fw, path_rv))
+print(locator(path_fw, path_rv))
 
 # print(record_F.annotations.keys())
 # dict_keys(["dye", "abif_raw", "sample_well", "run_finish", "machine_model", "run_start", "polymer"])
