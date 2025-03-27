14b5445bef51856d92bc632d7c66d784312ae803
PedroAlb-Blip <pedroalbendeaperez@gmail.com>
2025-03-27 19:28:19 +0100

    modified:   corrector.py

--- corrector.py
+++ corrector.py
@@ -10,6 +10,46 @@
 import ast
 import matplotlib.pyplot as plt
 
+#### PERHAPS A CONFIDENCE METRIC CAN BE CALCULATED AS THE VALUE OF EACH CHANNEL DIVIDED BY THE TOTAL AND CHOOSE ONWARDS FROM A THRESHOLD OF HIGH REPETITIONS
+def confidence(peaks, dol): 
+    conl = []
+    rm_base = []
+    intens = []
+    peak_1 = peaks[0]
+    peak_dist = []
+    for i in peaks:
+        peak_dist.append(i - peak_1)
+        peak_1 = i
+        sums = sum([dol[c][i] for c in dol.keys()])
+        intens.append(max([dol[c][i] for c in dol.keys()]))
+        try:
+            conme = max([dol[c][i]/sums for c in dol.keys()])
+        except ZeroDivisionError:
+            rm_base.append(peaks.index(i))
+        conl.append(conme)
+
+    return conl, intens, peak_dist, rm_base
+
+
+def rename(dol, guide):
+    tmp_lst = list(dol.keys())
+    for i in range(0, len(dol.keys())):
+        dol[guide[i]] = dol.pop(str(tmp_lst[i]))
+    return dol
+
+def peak_discovery(dol):
+    keys = list(dol.keys())
+    peaks_key = {}
+    lop = []
+    for key in keys:
+        key_list=[]
+        for i in range(2, len(dol[key])-2):
+            if dol[key][i] - dol[key][i-1] < 0 and dol[key][i-1] - dol[key][i-2] > 0: #and dol[key][i] != 0 and dol[key][i-1] != 0 and dol[key][i-2] != 0
+                key_list.append(i-1)
+        peaks_key[key] = key_list
+        lop = lop + peaks_key[key]
+    lop.sort()
+    return peaks_key, lop
 
 file = open("Sp101b-VP7.txt", "r")
 data = file.read()
@@ -31,54 +71,35 @@ C_fw = []; C_rv = []
 G_fw = []; G_rv = []
 T_fw = []; T_rv = []
 
-#### PERHAPS A CONFIDENCE METRIC CAN BE CALCULATED AS THE VALUE OF EACH CHANNEL DIVIDED BY THE TOTAL AND CHOOSE ONWARDS FROM A THRESHOLD OF HIGH REPETITIONS
-def confidence(peaks, dol): 
-    conl = []
-    rm_base = []
-    intens = []
-    for i in peaks:
-        sums = sum([dol[c][i] for c in dol.keys()])
-        intens.append(max([dol[c][i] for c in dol.keys()]))
-        try:
-            conme = max([dol[c][i]/sums for c in dol.keys()])
-        except ZeroDivisionError:
-            rm_base.append(peaks.index(i))
-        conl.append(conme)
-    return conl, intens, rm_base
 
-def peak_discovery(dol, guide):
-    tmp_lst = list(dol.keys())
-    for i in range(0, len(dol.keys())):
-        dol[guide[i]] = dol.pop(str(tmp_lst[i]))
-    keys = list(dol.keys())
-    peaks_key = {}
-    lop = []
-    for key in keys:
-        key_list=[]
-        for i in range(2, len(dol[key])-2):
-            if dol[key][i] - dol[key][i-1] < 0 and dol[key][i-1] - dol[key][i-2] > 0: #and dol[key][i] != 0 and dol[key][i-1] != 0 and dol[key][i-2] != 0
-                key_list.append(i)
-        peaks_key[key] = key_list
-        lop = lop + peaks_key[key]
-    lop.sort()
-    return peaks_key, lop
-
-all_peaks = peak_discovery(channels_fw, guide_fw)[1]
+all_peaks = peak_discovery(channels_fw)[1]
 new_peaks = all_peaks.copy()
 tmp_lst = list(ploc_fw).copy()
+peak_dist_bc = confidence(tmp_lst, channels_fw)[2]
+peak_dist_inv = confidence(new_peaks, channels_fw)[2]
 for i in all_peaks:
     for j in ploc_fw:
         if i in range(j-5, j+5) and i in new_peaks and j in tmp_lst:
             # print(i, j)
             try:
+                peak_dist_bc.pop(tmp_lst.index(j))
                 tmp_lst.remove(j)
+                peak_dist_inv.pop(new_peaks.index(i))
                 new_peaks.remove(i)
             except ValueError:
                 pass
 
-print(confidence(tmp_lst, channels_fw)[0], confidence(new_peaks, channels_fw)[0])
 
-print(confidence(tmp_lst, channels_fw)[1], confidence(new_peaks, channels_fw)[1])
+print(peak_dist_bc, peak_dist_inv)
+
+# print(confidence(tmp_lst, channels_fw)[0], confidence(new_peaks, channels_fw)[0])
+
+# print(confidence(tmp_lst, channels_fw)[1], confidence(new_peaks, channels_fw)[1])
+
+
+rename(channels_fw, guide_fw)
+
+
 
 # for i in range(locs[0][0], locs[0][1]):
 
